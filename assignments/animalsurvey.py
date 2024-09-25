eb58d8d2f07ec6dbd0bfeb15cd58ca109de6bb20
821335 <821335@mystma.org>
2024-09-25 09:31:45 -0500

    Animal notes

--- assignments/animalsurvey.py
+++ assignments/animalsurvey.py
@@ -1,9 +1,9 @@
-Q1= input("what is your favorite animal?\n")
-Q2= input("Where do Lions live?\n")
-Q3= input("What do Lions eat?\n")
-Q4= input("What's a fun fact about lions?/n")
-Q5= input("What color is a lion?\n")
-print("Your favorite animal is the " + Q1 = " because " + Q2 + ". 
+Q1= input("what is your favorite animal?\n")    #Asking what the animal is
+Q2= input("Where does the animal live?\n")      #Asking where the animal lives
+Q3= input("What does the animal eat?\n")       #What foods does the animal eat
+Q4= input("What's a fun fact about the animal?/n")  #intresting facts about the animal
+Q5= input("What color is the animal?\n")        #What does the animal look like
+print("Your favorite animal is the " + Q1 = " animal lives in the " + Q2 = ""
 
 
 
