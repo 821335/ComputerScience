e8d5e6201290de6d415741b1042ef9f0c73d6b2b
821335 <821335@mystma.org>
2024-09-25 09:19:43 -0500

    Animal assignment

---
+++ assignments/animalsurvey.py
@@ -0,0 +1,14 @@
+Q1= input("what is your favorite animal?\n")
+Q2= input("Where do Lions live?\n")
+Q3= input("What do Lions eat?\n")
+Q4= input("What's a fun fact about lions?/n")
+Q5= input("What color is a lion?\n")
+print("Your favorite animal is the " + Q1 = " because " + Q2 + ". 
+
+
+
+
+
+
+
+
---
+++ unit 2 - functions and variables/practice_sept-24.py
@@ -0,0 +1,11 @@
+print("Welcome to WORD SMASH!")
+word_one = input("Enter the first word!\n>")
+word_two = input("Enter the second word!\n>")
+print(word_one + word_two)
+
+
+
+
+   #DecLaration: Creating          #Assignment: Changing
+
+
