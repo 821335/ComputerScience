efc4c7c46464bd08912555bc5f91c0a6497955e9
821335 <821335@mystma.org>
2024-09-26 08:53:40 -0500

    converting data

--- assignments/animalsurvey.py
+++ assignments/animalsurvey.py
@@ -3,7 +3,7 @@ Q2= input("Where does the animal live?\n")      #Asking where the animal lives
 Q3= input("What does the animal eat?\n")       #What foods does the animal eat
 Q4= input("What's a fun fact about the animal?/n")  #intresting facts about the animal
 Q5= input("What color is the animal?\n")        #What does the animal look like
-print("Your favorite animal is the " + Q1 = " animal lives in the " + Q2 = ""
+print("Your favorite animal is the " + Q1 = " animal lives in the " + Q2 = "the animal eats " + Q3 = "A fact about the animal is " + Q4 "The color of the animal is " + Q5.
 
 
 
