89cd3167ac8479b30125985d3df425ed95dea2cf
821335 <821335@mystma.org>
2024-09-26 09:16:02 -0500

    Simple mad lib

---
+++ unit 2 - functions and variables/SimpleMadLib.py
@@ -0,0 +1,15 @@
+#Once upon a time, in a Big world, 
+#there lived a Cruel lion. 
+#Every day. the lion would run agressivly.
+#One dau, a large bear appeared
+#and ran the lion away
+#The bear was very large and angry.
+
+
+user_input = input("Once upon a time, in a Big world,")
+user_input = input("there lived a Cruel lion.")
+user_input = input("Every day, the lion would run agressivly.")
+user_input = input("One day, a large bear appeared")
+user_input = input("and ran the lion away")
+user_input = input("The bear was very large and angry.")
+print("Once upon a time, in a Big world" + "there lived a Cruel Lion." + "Every day, the lion would run agressivly." + "One day, a large bear appeared" + "and ran the lion away" + "The bear was very large and and angry.")
