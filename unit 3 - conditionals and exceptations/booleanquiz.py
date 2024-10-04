65598349f1f8e9f9a5dc0295d9c538fd98a5d65e
821335 <821335@mystma.org>
2024-10-04 09:35:50 -0500

    COMMIT_EDITMSG

---
+++ unit 3 - conditionals and exceptations/booleanquiz.py
@@ -0,0 +1,24 @@
+answer = int(input("give a whole number that is greater then 14.\n"))
+print(answer > 14)
+
+
+answer = int(input("give a whole number that is greater than or equal to 61.\n"))
+print(answer >= 61)
+
+answer = int(input("give me a whole number that is equal to 24.\n"))
+print(answer == 18) 
+
+answer = int(input("give me a whole number that is less than 37.\n"))
+print(answer < 37) 
+
+temperature = 61 
+if (temperature > 62):
+    print("It's a mild day!")
+else:
+    print("It's a cooler day!")
+
+answer = int(input("give me a whole number that is less than or equal to 134.\n"))
+print(answer <= 134)
+
+
+
