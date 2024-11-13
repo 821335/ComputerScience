01012133443e7274a15a7d34740ba42fb8f0f0de
821335 <821335@mystma.org>
2024-11-13 08:57:11 -0600

    COMMIT_EDITMSG

--- circuitplayground/blinky.py
+++ circuitplayground/blinky.py
@@ -1,29 +1,13 @@
-from adafruit_circuitplayground import cp
 import time
-# when the program finishes, the board enters a waiting state
-# flashes all green, then off
+from adafruit_circuitplayground import cp
+
+interval = 0.367
 
 while True:
-    cp.pixels[0] = (0, 1, 0)
-    cp.pixels[1] = (0, 1, 0)
-    cp.pixels[2] = (0, 1, 0)
-    cp.pixels[3] = (0, 1, 0)
-    cp.pixels[4] = (0, 1, 0)
-    cp.pixels[5] = (0, 1, 0)
-    cp.pixels[6] = (0, 1, 0)
-    cp.pixels[7] = (0, 1, 0)
-    cp.pixels[8] = (0, 1, 0)
-    cp.pixels[9] = (0, 1, 0)
-    print(1)
-    time.sleep(0.367)
-    print(2)
-    
+    cp.pixels.fill((0, 255, 0))
+    time.sleep(interval)
 
-    
-    
-    
+    cp.pixels.fill((0, 0, 0))
+    time.sleep(interval)
 
-
-
-
-    cp.pixels.brightness = 0.05
+    cp.pixels.brightness = 0.03
