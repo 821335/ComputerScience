de2b1e75c698893579868e4f46f1197ac0eb3fb1
821335 <821335@mystma.org>
2024-11-13 23:41:31 +0000

    COMMIT_EDITMSG

--- circuitplayground/flip_flop.py
+++ circuitplayground/flip_flop.py
@@ -16,3 +16,8 @@ while True:
                 
                 cp.pixels.brightness = 0.02
                 # not finished
+
+                #change line 8 to (0, 255, 0)
+                #change line 10 to (0, 0, 0)
+                #change line 13 to (0, 0, 0)
+                #change line 15 to (0, 255, 0)
