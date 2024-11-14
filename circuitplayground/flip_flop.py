a852d7d6ace01cc39978d2ba6a6a7bc0e8cbb6ae
821335 <821335@mystma.org>
2024-11-14 09:35:34 -0600

    COMMIT_EDITMSG

--- circuitplayground/flip_flop.py
+++ circuitplayground/flip_flop.py
@@ -1,18 +1,16 @@
 from adafruit_circuitplayground import cp
 
-green = (0, 255, 0)
-
 while True:
     if cp.switch:
         for i in range (5):
-            cp.pixels[i] = (0, 0, 0)
+            cp.pixels[i] = (0, 255, 0)
         for i in range(5,10):
-            cp.pixels[i] = green
+            cp.pixels[i] = (0, 0, 0)
         else:
                 for i in range (5):
-                    cp.pixels[i] = green
-                for i in range (5, 10):
                     cp.pixels[i] = (0, 0, 0)
+                for i in range (5, 10):
+                    cp.pixels[i] = (0, 255, 0)
                 
                 cp.pixels.brightness = 0.02
-                # not finished
+                
